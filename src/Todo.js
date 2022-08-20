b4cd91ad396a1023af285adb7a22c21b244ae107
seongyeon <ysy2565@naver.com>
2022-08-20 22:56:39 +0900

    style: 추가 및 삭제 스타일 추가

--- src/Todo.js
+++ src/Todo.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { observer } from "mobx-react";
-import TodoTemplate from "./components/TodoTemplate";
 
 const Todo = observer(({ todos }) => (
   <div>
-    <TodoTemplate>
-      {todos.todos.map((todo) => (
-        <div key={todo.id}>{todo.todo}</div>
-      ))}
-    </TodoTemplate>
+    <h1>To Do</h1>
+    <form>
+      <input placeholder="할 일을 입력하세요" />
+      <button type="submit" onClick={todos.create}>
+        추가하기
+      </button>
+    </form>
+    {todos.todos.map((todo) => (
+      <div key={todo.id}>
+        {todo.todo}
+        <button onClick={todos.remove}>삭제하기</button>
+      </div>
+    ))}
   </div>
 ));
 
