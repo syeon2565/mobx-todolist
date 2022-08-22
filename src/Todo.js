7d90d822552bb27f35cc34dba424ee18ccbffd6a
seongyeon <ysy2565@naver.com>
2022-08-22 18:56:51 +0900

    style: todo done line-through설정

--- src/Todo.js
+++ src/Todo.js
@@ -12,7 +12,11 @@ const Todo = observer(({ todos }) => (
     {todos.todos.map((todo) => (
       <div key={todo.id}>
         <input type="checkbox" checked={todo.checked} />
-        {todo.todo}
+        {todo.checked ? (
+          <span style={{ textDecoration: "line-through" }}>{todo.todo}</span>
+        ) : (
+          <span>{todo.todo}</span>
+        )}
         <button onClick={todos.remove}>삭제하기</button>
       </div>
     ))}
