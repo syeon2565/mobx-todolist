3b0c876a19781af928c51140633520334e337209
seongyeon <ysy2565@naver.com>
2022-08-23 12:23:17 +0900

    feat: todo 삭제 기능

--- src/Todo.js
+++ src/Todo.js
@@ -1,26 +1,37 @@
 import { observer } from "mobx-react";
+import TodoInsert from "./components/TodoInsert";
 
-const Todo = observer(({ todos }) => (
-  <div>
-    <h1>To Do</h1>
-    <form>
-      <input placeholder="할 일을 입력하세요" />
-      <button type="submit" onClick={todos.create}>
-        추가하기
-      </button>
-    </form>
-    {todos.todos.map((todo) => (
-      <div key={todo.id}>
-        <input type="checkbox" checked={todo.checked} />
-        {todo.checked ? (
-          <span style={{ textDecoration: "line-through" }}>{todo.todo}</span>
-        ) : (
-          <span>{todo.todo}</span>
-        )}
-        <button onClick={todos.remove}>삭제하기</button>
-      </div>
-    ))}
-  </div>
-));
+const Todo = observer(({ todos }) => {
+  return (
+    <div>
+      <h1>To Do</h1>
+      <TodoInsert todos={todos} />
+      {todos.todos.map((todo) => (
+        <label key={todo.id}>
+          <br />
+          <input
+            type="checkbox"
+            checked={todo.checked}
+            onChange={() => {
+              todos.toggle(todo.id);
+            }}
+          />
+          {todo.checked ? (
+            <span style={{ textDecoration: "line-through" }}>{todo.todo}</span>
+          ) : (
+            <span>{todo.todo}</span>
+          )}
+          <button
+            onClick={() => {
+              todos.remove(todo.id);
+            }}
+          >
+            삭제하기
+          </button>
+        </label>
+      ))}
+    </div>
+  );
+});
 
 export default Todo;
